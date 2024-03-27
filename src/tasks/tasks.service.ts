11bb3f0dc930c5bebfb75e7f147d51cd4d104105
One1Van1 <sa4kov.2004@gmail.com>
2024-03-26 22:53:19 -0300

    update create method

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -13,7 +13,17 @@ export class TasksService {
   ) {}
 
   async create(createTaskDto: CreateTaskDto): Promise<Task> {
-    return await this.taskRepository.save(createTaskDto);
+    // Устанавливаем значение completed в true по умолчанию, если оно не было указано
+    const completed =
+      createTaskDto.completed !== undefined ? createTaskDto.completed : false;
+
+    // Создаем новый экземпляр задачи, передавая указанный title и значение completed
+    const task = new Task();
+    task.title = createTaskDto.title;
+    task.completed = completed;
+
+    // Сохраняем задачу в репозитории и возвращаем ее
+    return await this.taskRepository.save(task);
   }
 
   async findAll(): Promise<Task[]> {
